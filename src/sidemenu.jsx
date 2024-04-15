b761f48edc92aa9e1cc430caf351a8ecac56f282
Ameen-codes <alameenakanbi@gmail.com>
2024-04-15 15:00:21 +0100

    third commit

--- src/sidemenu.jsx
+++ src/sidemenu.jsx
@@ -11,7 +11,7 @@ function Sidemenu(){
 187.2c-24 18-38.2 46.1-38.4 76.1L339.3 367.1zm-19.6 25.4l-116-104.4C143.9 290.3 96 339.6 96 400c0 3.9 .2 7.8 .6 11.6C98.4 
 429.1 86.4 448 68.8 448H64c-17.7 0-32 14.3-32 32s14.3 32 32 32H208c61.9 0 112-50.1 112-112c0-2.5-.1-5-.2-7.5z"/>
 </svg>My NFTs</a></li>
-                <li><img src={require("./media/transactions.png")} alt="" id="icon"/><a href="">Transactions</a></li>
+                <li><a href=""><img src={require("./media/transactions.png")} alt="" id="icon"/>Transactions</a></li>
             </ul>
             
 
