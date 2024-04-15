1766916ea165cd7e8b78e70bf04e068c4ecdcc2f
Ameen-codes <alameenakanbi@gmail.com>
2024-04-15 18:54:10 +0100

    6th

--- src/header.jsx
+++ src/header.jsx
@@ -1,29 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import WithdrawFunds from "./withdraw";
 
-function Header(){
-    
-    return(
-        <nav id="top_nav">
-            
-            <h1>Artisan</h1>
-            
-            <ul>
-                <li><a href="">Resources</a></li>
-                <li><a href="">Portfolio</a></li>
-                <li><a href="">Market</a></li>
-                <li><a href="">Learn More</a></li>
-            </ul>
+function Header() {
+  const [accounts, setAccounts] = useState([]);
 
-            <span id="cw"> 
+  const connectMetaMask = async () => {
+    if (window.ethereum) {
+      try {
+        await window.ethereum.request({ method: "eth_requestAccounts" });
+        const accounts = await window.ethereum.request({
+          method: "eth_accounts",
+        });
+        setAccounts(accounts);
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      console.error("MetaMask not installed");
+    }
+  };
+  return (
+    <nav id="top_nav">
+      <h1>Artisan</h1>
 
-                <img src={require("./media/flash.png")} alt="img" /> 
+      <ul>
+        <li>
+          <a href="">Resources</a>
+        </li>
+        <li>
+          <a href="">Portfolio</a>
+        </li>
+        <li>
+          <a href="">Market</a>
+        </li>
+        <li>
+          <a href="">Learn More</a>
+        </li>
+      </ul>
+      {accounts.length >= 1 &&
+        accounts.map((el, index) => {
+          return <span key={index}>{el}</span>;
+        })}
 
-                <a href="">Connect Wallet</a> 
+      <WithdrawFunds account={accounts[1]} />
+      <span id="cw">
+        <img src={require("./media/flash.png")} alt="img" />
 
-            </span>
-
-        </nav>
-    )
+        <button onClick={connectMetaMask}>Connect Wallet</button>
+      </span>
+    </nav>
+  );
 }
 
-export default Header ;
+export default Header;
---
+++ src/withdraw.jsx
@@ -0,0 +1,52 @@
+import React, { useState,useEffect } from 'react';
+import Web3 from 'web3';
+
+function WithdrawFunds({ account }) {
+    const sepoliaRPC = 'https://sepolia.io/rpc';
+    const web3 = new Web3(sepoliaRPC)
+  const [withdrawAmount, setWithdrawAmount] = useState('');
+  const [recipientAddress, setRecipientAddress] = useState('');
+  const [transactionHash, setTransactionHash] = useState('');
+
+  const handleWithdraw = async () => {
+    try {
+      
+
+      // Convert withdrawal amount to Wei
+      const amountInWei = web3.utils.toWei(withdrawAmount,'ether');
+
+      // Send transaction
+      const transaction = await web3.eth.sendTransaction({
+        from: account,
+        to: recipientAddress,
+        value: amountInWei,
+      });
+
+      setTransactionHash(transaction.transactionHash);
+    } catch (error) {
+      console.error('Error withdrawing funds:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Withdraw Funds</h2>
+      <label>
+        Withdraw Amount (ETH):
+        <input type="number" value={withdrawAmount} onChange={(e) => setWithdrawAmount(e.target.value)} />
+      </label>
+      <label>
+        Recipient Address:
+        <input type="text" value={recipientAddress} onChange={(e) => setRecipientAddress(e.target.value)} />
+      </label>
+      <button onClick={handleWithdraw}>Withdraw</button>
+      {transactionHash && (
+        <p>
+          Transaction submitted. Transaction Hash: <a href={`https://etherscan.io/tx/${transactionHash}`} target="_blank" rel="noopener noreferrer">{transactionHash}</a>
+        </p>
+      )}
+    </div>
+  );
+}
+
+export default WithdrawFunds;
